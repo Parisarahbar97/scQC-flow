c9643274f65625239a6b3938cd727ae36f2467e2
PARISA RAHBAR <parisa.rahbar22@imperial.ac.uk>
2025-12-02 22:59:15 +0000

    update

--- scQC-flow_command.sh
+++ scQC-flow_command.sh
@@ -7,6 +7,7 @@ nextflow drop Parisarahbar97/scQC-flow
 
 
 
+# BONN epileptic samples:
 nextflow run Parisarahbar97/scQC-flow -r main \
   --mapping_dirs /rds/general/user/pr422/projects/puklandmarkproject/live/Users/Parisa/EPILEP/diseased/qc/input/mapping_dirs.csv \
   --outputDir /rds/general/user/pr422/projects/puklandmarkproject/live/Users/Parisa/EPILEP/diseased/qc/output_latest \
@@ -15,7 +16,7 @@ nextflow run Parisarahbar97/scQC-flow -r main \
   -profile imperial \
   -with-report -with-trace -with-timeline
 
-
+# MRC control samples:
 nextflow run Parisarahbar97/scQC-flow -r main \
   --mapping_dirs /rds/general/user/pr422/projects/puklandmarkproject/live/Users/Parisa/EPILEP/healthy/qc/input/mapping_dirs.csv \
   --outputDir /rds/general/user/pr422/projects/puklandmarkproject/live/Users/Parisa/EPILEP/healthy/qc/output_latest \
@@ -24,4 +25,11 @@ nextflow run Parisarahbar97/scQC-flow -r main \
   -profile imperial \
   -with-report -with-trace -with-timeline
 
-
+# UCL epileptic samples:
+nextflow run Parisarahbar97/scQC-flow -r main \
+  --mapping_dirs /rds/general/user/pr422/projects/puklandmarkproject/live/Users/Parisa/EPILEP/diseased/UCL/qc/input/mapping_dirs.csv \
+  --outputDir   /rds/general/user/pr422/projects/puklandmarkproject/live/Users/Parisa/EPILEP/diseased/UCL/qc/output \
+  -w "$NXF_WORK" \
+  --cellbender true --gpu true \
+  -profile imperial \
+  -with-report -with-trace -with-timeline
